ab202ed5108070ee606e3dbdbd56adcf3fede86f
Bernd <berndlosert@gmail.com>
2021-11-29 17:33:38 +0100

    Saving src/test.lean

--- src/test.lean
+++ src/test.lean
@@ -11,12 +11,12 @@ universes u v w
 def dot {α : Type u} (x : α) : filter α := principal {x}
 
 class convergence_space (α : Type u) :=
-(conv        : filter α → α → Prop)
-(dot_conv    : ∀ (x : α), conv (dot x) x)
-(subfil_conv : ∀ (x : α) (F G : filter α), F ≤ G ∧ conv F x → conv G x)
-(inter_conv  : ∀ (x : α) (F G : filter α), conv F x → conv (F ⊓ dot x) x)
+(lim        : filter α → set α)
+(dot_conv    : ∀ (x : α), x ∈ lim (dot x))
+(subfil_conv : ∀ (x : α) (F G : filter α), F ≤ G ∧ x ∈ lim F → x ∈ lim G)
+(inter_conv  : ∀ (x : α) (F G : filter α), x ∈ lim F → x ∈ lim (F ⊓ dot x))
 
 open convergence_space
 
 structure continuous {α β : Type*} (f : α → β) [convergence_space α] [convergence_space β] : Prop :=
-(filter_conv : ∀ (F : filter α) (x : α), conv F x → conv (map f F) (f x))
+(filter_conv : ∀ (F : filter α) (x : α), x ∈ lim F → f x ∈ lim (map f F))
