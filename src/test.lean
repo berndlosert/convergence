fa1b84c2e68c245faa073684e03cd55f1333606e
Bernd <berndlosert@gmail.com>
2021-12-04 17:31:30 +0100

    Saving src/test.lean

--- src/test.lean
+++ src/test.lean
@@ -17,21 +17,21 @@ by simp [dot]
 class convergence_space (α : Type u) :=
 (conv : filter α → α → Prop)
 (dot_conv : ∀ {x : α}, conv (dot x) x)
-(le_conv : ∀ {x : α} {F G : filter α}, F ≤ G → conv F x → conv G x)
-(sup_conv : ∀ {x : α} {F G : filter α}, conv F x → conv G x → conv (F ⊔ G) x)
+(le_conv : ∀ {x : α} {l l' : filter α}, l ≤ l' → conv l x → conv l' x)
+(sup_conv : ∀ {x : α} {l l' : filter α}, conv l x → conv l' x → conv (l ⊔ l') x)
 
 open convergence_space
 
-def lim [convergence_space α] (F : filter α) : set α := set_of (conv F)
+def lim [convergence_space α] (l : filter α) : set α := set_of (conv l)
 
 structure continuous [convergence_space α] [convergence_space β] (f : α → β) : Prop :=
-(filter_conv : ∀ {x : α} {F : filter α}, conv F x → conv (map f F) (f x))
+(filter_conv : ∀ {x : α} {l : filter α}, conv l x → conv (map f l) (f x))
 
 class hausdorff_space [convergence_space α] : Prop :=
-(hausdorff_pred : ∀ (F : filter α), subsingleton (lim F))
+(hausdorff_pred : ∀ (l : filter α), subsingleton (lim l))
 
 def induced (f : α → β) (t : convergence_space β) : convergence_space α := {
-  conv := λ F x, conv (map f F) (f x),
+  conv := λ l x, conv (map f l) (f x),
   dot_conv := begin
     intro,
     simp [map_dot],
@@ -39,15 +39,15 @@ def induced (f : α → β) (t : convergence_space β) : convergence_space α :=
   end,
   le_conv := begin
     assume x : α,
-    assume F G : filter α,
-    assume h0 : F ≤ G,
-    assume h1 : conv (map f F) (f x),
-    have h : map f F ≤ map f G, apply map_mono h0,
-    apply le_conv h h1
+    assume l l' : filter α,
+    assume h₀ : l ≤ l',
+    assume h₁ : conv (map f l) (f x),
+    have h₂ : map f l ≤ map f l', apply map_mono h₀,
+    apply le_conv h₂ h₁
   end,
   sup_conv := begin
-    intros x F G h0 h1,
+    intros x l l' h₀ h₁,
     simp [map_sup],
-    apply sup_conv h0 h1
+    apply sup_conv h₀ h₁
   end
 }
