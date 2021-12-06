ac1b64aef4b72e4cf0597423e8819ef5fc8f440b
Bernd <berndlosert@gmail.com>
2021-12-06 19:22:42 +0100

    Saving src/test.lean

--- src/test.lean
+++ src/test.lean
@@ -33,42 +33,54 @@ def induced (f : a -> b) (t : convergence_space b) : convergence_space a := {
     apply pure_conv
   end,
   le_conv := begin
-    intros x l l' h₀ h₁,
-    have h₂ : map f l <= map f l',
-      apply map_mono h₀,
-    apply le_conv h₂ h₁
+    intros x l l' h0 h1,
+    have h2 : map f l <= map f l',
+      apply map_mono h0,
+    apply le_conv h2 h1
   end,
   sup_conv := begin
-    intros x l l' h₀ h₁,
+    intros x l l' h0 h1,
     simp [map_sup],
-    apply sup_conv h₀ h₁,
+    apply sup_conv h0 h1,
   end
 }
 
+inductive coinduced_conv (f : a -> b) (l' : filter b) (y : b) : Prop
+| pure_case (hp : l' <= pure y) : coinduced_conv
+| impure_case [convergence_space a] (l : filter a) (x : a) (hlt : l' <= map f l) (hfx : y = f x) (hconv : conv l x) : coinduced_conv
+
 def coinduced (f : a -> b) (t : convergence_space a) : convergence_space b := {
-  conv := fun l' y, l' <= pure y \/ (exists x l, l' <= map f l /\ y = f x /\ conv l x),
+  conv := coinduced_conv f,
+  pure_conv := fun y, coinduced_conv.pure_case (le_refl (pure y)),
+  le_conv := sorry,
+  sup_conv := sorry,
+}
+/-
+def coinduced (f : a -> b) (t : convergence_space a) : convergence_space b := {
+  conv := fun l' y, (l' <= pure y) \/ (exists x l, l' <= map f l /\ y = f x /\ conv l x),
   pure_conv := begin
     intro,
     simp [and.left]
   end,
   le_conv := begin
-    intros y l₁ l₂ h₀ h₁,
-    exact or.elim h₁
-      (assume h, or.inl (trans h₀ h))
+    intros y l1 l2 h0 h1,
+    exact or.elim h1
+      (assume h, or.inl (trans h0 h))
       (assume h, match h with ⟨x, l, hm, hf, hc⟩ :=
-        or.inr ⟨x, l, trans h₀ hm, hf, hc⟩
+        or.inr ⟨x, l, trans h0 hm, hf, hc⟩
       end)
   end,
   sup_conv := begin
-    intros y l₁ l₂ h₀ h₁,
-    exact or.elim h₀
-      (or.elim h₁
+    intros y l1 l2 h0 h1,
+    exact or.elim h0
+      (or.elim h1
         (assume ha hb, or.inl (sup_le_iff.mpr ⟨hb, ha⟩))
 	(assume ha hb, sorry))
-      (or.elim h₁ sorry sorry)
-    --  (or.elim h₁
+      (or.elim h1 sorry sorry)
+    --  (or.elim h1
     --    (assume ha hb, or.inl (sup_lt_iff.mpr (ha, hb)))
     --    _)
-    --  (or.elim h₁ _ _)
+    --  (or.elim h1 _ _)
   end
 }
+-/
