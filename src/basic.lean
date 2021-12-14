ac54f87b8bc79c33be9abf4620462b99bf495a7f
Bernd <berndlosert@gmail.com>
2021-12-14 18:08:03 +0100

    Saving basic.lean

--- src/basic.lean
+++ src/basic.lean
@@ -5,7 +5,7 @@ import algebra.support
 noncomputable theory
 open set filter classical
 open_locale classical filter
-open has_sup
+open has_sup has_mem
 
 variables {a b : Type*}
 
@@ -31,10 +31,30 @@ class pseudotopological_space (a : Type*) extends limit_space a :=
 
 open convergence_space kent_convergence_space limit_space
 
+-------------------------------------------------------------------------------
+-- Basic definitions
+-------------------------------------------------------------------------------
+
 def lim [convergence_space a] (l : filter a) : set a := set_of (conv l)
+
+def adheres [convergence_space a] (l : filter a) (x : a) : Prop :=
+exists l' <= l, conv l' x
+
+def adh [convergence_space a] (l : filter a) : set a := set_of (adheres l)
+
+def is_open [convergence_space a] (s : set a) : Prop :=
+forall {l : filter a} {x : a}, mem x s -> conv l x -> mem s l
+
+def is_closed [convergence_space a] (s : set a) : Prop :=
+forall {l : filter a} {x : a}, mem s l -> conv l x -> mem x s
 
 structure continuous [convergence_space a] [convergence_space b] (f : a -> b) : Prop :=
 (filter_conv : forall {x : a} {l : filter a}, conv l x -> conv (map f l) (f x))
+
+
+-------------------------------------------------------------------------------
+-- Misc.
+-------------------------------------------------------------------------------
 
 class hausdorff_space [convergence_space a] : Prop :=
 (hausdorff_prop : forall (l : filter a) [ne_bot l], subsingleton (lim l))
