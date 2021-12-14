8bff1569bbb22befeb558c6528edb26963da1ff6
Bernd <berndlosert@gmail.com>
2021-12-14 17:39:48 +0100

    Saving basic.lean

--- src/basic.lean
+++ src/basic.lean
@@ -9,10 +9,16 @@ open has_sup
 
 variables {a b : Type*}
 
+-------------------------------------------------------------------------------
+-- Definition of convergence space and specializations thereof
+-------------------------------------------------------------------------------
+
 class convergence_space (a : Type*) :=
 (conv : filter a -> a -> Prop)
 (pure_conv : forall {x : a}, conv (pure x) x)
 (le_conv : forall {x : a} {l l' : filter a}, l <= l' -> conv l' x -> conv l x) -- l <= l' means l' ⊆ l
+
+attribute [ext] convergence_space
 
 class kent_convergence_space (a : Type*) extends convergence_space a :=
 (kent_conv : forall {x : a} {l : filter a}, conv l x -> conv (sup l (pure x)) x)
@@ -20,10 +26,10 @@ class kent_convergence_space (a : Type*) extends convergence_space a :=
 class limit_space (a : Type*) extends kent_convergence_space a :=
 (sup_conv : forall {x : a} {l l' : filter a}, conv l x -> conv l' x -> conv (sup l l') x) -- l ⊔ l' means l ∩ l'
 
-open convergence_space kent_convergence_space limit_space
+class pseudotopological_space (a : Type*) extends limit_space a :=
+(ultra_conv : forall {x : a} {l : filter a}, (forall {u : ultrafilter a}, conv u.to_filter x) -> conv l x)
 
-@[ext] theorem convergence_space_eq : forall {p q : convergence_space a}, p.conv = q.conv -> p = q
-| (convergence_space.mk pconv _ _) (convergence_space.mk qconv _ _) rfl := rfl
+open convergence_space kent_convergence_space limit_space
 
 def lim [convergence_space a] (l : filter a) : set a := set_of (conv l)
 
