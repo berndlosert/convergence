44bcc341850c48a63cdef2f9a900cb6240e2baf5
Bernd <berndlosert@gmail.com>
2022-01-28 22:33:02 +0100

    Saving src/limit_space/basic.lean

--- src/limit_space/basic.lean
+++ src/limit_space/basic.lean
@@ -4,7 +4,7 @@ import algebra.support
 import kent_convergence_space.basic
 
 noncomputable theory
-open set filter classical
+open set filter classical kent_convergence_space
 open_locale classical filter
 
 variables {X Y : Type*}
@@ -13,11 +13,10 @@ variables {X Y : Type*}
 -- Definition
 -------------------------------------------------------------------------------
 
-structure limit_space (X : Type*) extends kent_convergence_space X :=
+class limit_space (X : Type*) extends kent_convergence_space X :=
 (sup_converges : ∀ {x ℱ 𝒢}, converges ℱ x -> converges 𝒢 x -> converges (ℱ ⊔ 𝒢) x) -- ℱ ⊔ 𝒢 means ℱ ∩ 𝒢
 
 attribute [ext] limit_space
-attribute [class] limit_space
 
 open limit_space
 
