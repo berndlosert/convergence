611e9da05cb2464e31f9c5f9d863caa850058b1a
Bernd Losert <berndlosert@gmail.com>
2023-09-20 08:21:26 +0000

    WIP

--- lakefile.lean
+++ lakefile.lean
@@ -9,6 +9,6 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib convergence {
-  roots := #[`src]
+lean_lib Convergence {
+  roots := #[`Convergence]
 }
